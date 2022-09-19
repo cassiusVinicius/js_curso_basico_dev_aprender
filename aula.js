951c115dd22b73a4702e68324afe2a929561a45e
Cassius Vinicius <cassiusvinicius300@gmail.com>
2022-09-19 16:44:30 -0300

    um exemplo de objeto e um exemplo de array

--- aula.js
+++ aula.js
@@ -1,1 +1,15 @@
-console.log('Uma mensagem no console')
+//objeto
+let pessoa = {
+    nome: 'Rafael',
+    idade: 25,
+    estaAprovado: true,
+    sobrenome: 'de Souza',
+    cidade: 'Natal'
+}
+
+//array
+let amigo = [
+    'Vinícius', 25, 'Florianópolis'
+]
+
+console.log(amigo)
